9cad6557aef483e392950769e7cec57da9febe10
Fikry C. Farenza <vickyfarenza@gmail.com>
2024-12-05 21:09:41 +0700

    🖇️Route auth & dashboard

---
+++ app/Livewire/Home/Dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Livewire\Home;
+
+use Livewire\Component;
+
+class Dashboard extends Component {
+    public function render() {
+        return view('home.dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('login', \App\Livewire\Auth\Login::class)->name('login');
+Route::get('register', \App\Livewire\Auth\Register::class)->name('register');
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
+    Route::get('/', \App\Livewire\Home\Dashboard::class)->name('dashboard');
+
 });
