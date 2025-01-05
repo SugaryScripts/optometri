85cbcfd742be861196977c599bbd09c1a79d64d4
Fikry C. Farenza <vickyfarenza@gmail.com>
2025-01-06 03:16:13 +0700

    📦️Add debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,6 +12,9 @@ class AppServiceProvider extends ServiceProvider {
         if (!$this->app->environment('production')) {
             $this->app->register('App\Providers\FakerServiceProvider');
         }
+
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('Debugbar', \Barryvdh\Debugbar\Facades\Debugbar::class);
     }
 
     /**
